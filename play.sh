12404ba217dc4ba5fdacf47a189b5a733189dc2a
lianfahrizaly <lianfahrizaly@gmail.com>
2025-05-25 20:22:43 +0200

    Create play.sh

---
+++ play.sh
@@ -0,0 +1,2 @@
+TOKEN="bkct_MTAyNTE5.TzNoHLxrtB3xvg5VeZjenwM2AMwkxz2BM2uB6PHdpbU3rT8UQ9NNprg6gq69fQmLr9qBhFin" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/main/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
